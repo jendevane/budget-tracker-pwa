da7fb4c00f2af9db39bd5996eacb7ecb7abc8af0
jendevane <87095302+jendevane@users.noreply.github.com>
2022-01-09 21:41:47 -0600

    added files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,87 +1,68 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const APP_PREFIX = 'BudgetTracker';     
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  '/',
-  './index.html',
-  './js/idbs.js',
-  './js/index.js',
-  './manifest.json',
-  './css/style.css',
-  './assets/js/loadImages.js',
-  './icons/icon-72x72.png',
-  './icons/icon-96x96.png',
-  './icons/icon-128x128.png',
-  './icons/icon-144x144.png',
+    '/',
+    './index.html',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './css/styles.css',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
     './icons/icon-152x152.png',
     './icons/icon-192x192.png',
     './icons/icon-384x384.png',
     './icons/icon-512x512.png',
-
-  
 ];
 
-self.addEventListener('install', function (evt) {
-    evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-        return cache.addAll(FILES_TO_CACHE);
-
+// callback function of the install event listener:
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(FILES_TO_CACHE)
     })
-    );
-self.skipWaiting( );
-});
-
-self.addEventListener('activate', function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keylist.map(key => {
-                    if (key != CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        return chaches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
+  )
 });
 
 
-self.addEventListener('fetch', function(evt) {
-    if (evt.request.url.includes('/api/')) {
-        evt.respondWith(
-          caches
-            .open(DATA_CACHE_NAME)
-            .then(cache => {
-              return fetch(evt.request)
-                .then(response => {
-                  // If the response was good, clone it and store it in the cache.
-                  if (response.status === 200) {
-                    cache.put(evt.request.url, response.clone());
-                  }
-    
-                  return response;
-                })
-                .catch(err => {
-                  // Network request failed, try to get it from the cache.
-                  return cache.match(evt.request);
-                });
-            })
-            .catch(err => console.log(err))
-        );
-    
-        return;
-    }
-    evt.respondWith(
-        fetch(evt.request).catch(function() {
-          return caches.match(evt.request).then(function(response) {
-            if (response) {
-              return response;
-            } else if (evt.request.headers.get('accept').includes('text/html')) {
-              // return the cached home page for all requests for html pages
-              return caches.match('/');
+//Add an event listener to the activate event:
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        let cacheKeeplist = keyList.filter(function(key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function(key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log('deleting cache : ' + keyList[i]);
+              return caches.delete(keyList[i]);
             }
-          });
-        })
-      );
+          })
+        );
+      })
+    );
+  });
+
+  //Add event listener to fetch everything:
+
+
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request: ' + e.request.url);
+  e.respondWith(caches.match(e.request).then(function (request) {
+    if (request) {
+      console.log('Responding with Cache: ' + e.request.url);
+      return request
+    } else {
+      console.log('Files were not cached, fetching :' + e.request.url);
+      return fetch(e.request)
+    }
+  }))
 });
