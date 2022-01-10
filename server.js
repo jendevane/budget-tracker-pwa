adb26bf991b15414a536eea7bb27358a09c3ed60
jendevane <87095302+jendevane@users.noreply.github.com>
2022-01-10 01:52:11 -0600

    added files

--- server.js
+++ server.js
@@ -16,10 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Cluster0',
+  {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  },
+ );
 
 // routes
 app.use(require("./routes/api.js"));
