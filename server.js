f181476436a150bd3516ea436f0aa57e7b249e2b
jendevane <87095302+jendevane@users.noreply.github.com>
2022-01-10 02:05:06 -0600

    added files

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/Cluster0',
+  process.env.MONGODB_URI || 'mongodb://localhost/PWA',
   {
   useNewUrlParser: true,
   useUnifiedTopology: true,
