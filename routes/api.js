11190dcc44b82532c7da2a51469d7d6728748590
jendevane <87095302+jendevane@users.noreply.github.com>
2022-01-10 21:43:38 -0600

    troubleshooting

--- routes/api.js
+++ routes/api.js
@@ -1,32 +1,39 @@
 const router = require("express").Router();
-const Transaction = require("../models/transaction.js");
+const Transaction = require("../models/transaction.js")
 
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
+      console.log(err);
       res.status(404).json(err);
     });
 });
 
-router.post("/api/transaction/bulk", ({body}, res) => {
+router.post("/api/transaction/bulk", ({ body }, res) => {
   Transaction.insertMany(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
+      console.log(err);
       res.status(404).json(err);
     });
 });
 
 router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
+  console.log('xxxxx');
+  console.log(Transaction);
+  // console.log(req);
+  //console.log(res);
+  Transaction.find({}).sort({ date: -1 })
     .then(dbTransaction => {
       res.json(dbTransaction);
     })
     .catch(err => {
+      console.log(err);
       res.status(404).json(err);
     });
 });
