dbd68fb27abb028732ccbd49b1b8872c162a296f
jendevane <87095302+jendevane@users.noreply.github.com>
2022-01-10 18:12:58 -0600

    added to ibds

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,10 @@
 
-    
+ const indexedDB =
+ window.indexedDB ||
+ window.mozIndexedDB ||
+ window.webkitIndexedDB ||
+ window.msIndexedDB ||
+ window.shimIndexedDB;   
 let db;
 
 const request = indexedDB.open('budget-tracker', 1);
